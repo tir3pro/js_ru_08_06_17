21345830a07e6cd27b499c71c73b9b205ac2309c
Yevhen <yevhen.kryvsun@oracle.com>
2017-12-02 12:37:18 +0200

    Add refs

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Yevhen Kryvsun on 12/2/2017.
+ */
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import ArticleList from './ArticleList'
+import ArticlesChart from './ArticlesChart'
+
+class App extends Component {
+    static propTypes = {
+
+    };
+
+    render() {
+        return (
+            <div>
+                <ArticleList articles = {this.props.articles} />
+                <ArticlesChart articles = {this.props.articles} />
+            </div>
+        )
+    }
+}
+
+export default App
---
+++ src/components/ArticlesChart.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Yevhen Kryvsun on 12/2/2017.
+ */
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+class ArticlesChart extends Component {
+    static propTypes = {
+
+    };
+
+    componentDidMount() {
+        //d3 works with this.refs.chart
+    }
+
+    componentWillReceiveProps(nextProps) {
+        //update chart for new articles
+    }
+
+    render() {
+        return (
+            <div ref = 'chart'></div>
+        )
+    }
+
+    componentWillUnmount() {
+        //do some cleanup
+    }
+}
+
+export default ArticlesChart
+
