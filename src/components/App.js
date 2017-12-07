add43cd67efba5b9450606274b47796f62d614df
Yevhen <yevhen.kryvsun@oracle.com>
2017-12-07 19:14:23 +0200

    Add UserForm

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,19 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import ArticleList from './ArticleList'
 import ArticlesChart from './ArticlesChart'
+import UserForm from './UserForm'
 
 class App extends Component {
     static propTypes = {
         articles: PropTypes.array.isRequired
     };
 
+
+
     render() {
         return (
             <div>
+                <UserForm />
                 <ArticleList articles = {this.props.articles} />
                 <ArticlesChart articles = {this.props.articles} />
             </div>
