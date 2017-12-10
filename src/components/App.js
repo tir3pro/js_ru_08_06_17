2f22b7da0804a52de442a00412d7907432671efd
tir3pro <tir3pro@gmail.com>
2017-12-10 12:41:08 +0200

    Add react-select

--- src/components/App.js
+++ src/components/App.js
@@ -6,23 +6,37 @@ import PropTypes from 'prop-types'
 import ArticleList from './ArticleList'
 import ArticlesChart from './ArticlesChart'
 import UserForm from './UserForm'
+import Select from 'react-select'
+import 'react-select/dist/react-select.css';
 
 class App extends Component {
     static propTypes = {
         articles: PropTypes.array.isRequired
     };
 
-
+    state = {
+        selection: null
+    }
 
     render() {
+        const options = this.props.articles.map(aricle => {
+            return {
+                label: aricle.title,
+                value: aricle.id
+            }
+        })
+
         return (
             <div>
                 <UserForm />
+                <Select options = {options} value = {this.state.selection} onChange = {this.changeSelection}/>
                 <ArticleList articles = {this.props.articles} />
                 <ArticlesChart articles = {this.props.articles} />
             </div>
         )
     }
+
+    changeSelection = selection => this.setState({selection})
 }
 
 export default App
